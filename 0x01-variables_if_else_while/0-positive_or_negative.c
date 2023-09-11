48aeaca58f6b10907d12424eb5250d008c2cf5b7
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-09-11 08:17:03 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
 /**
  * main - create random numbers to detect positive and negative
  *
  * Return: always 0
- *
  */
+
 int main(void)
 {
 	int n;
