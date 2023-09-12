ceab6a5c7bd2e55c3e92b6d038a3aa57ae7a328f
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-09-12 13:35:58 -0700

    0-putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,13 +2,14 @@
 
 /**
  * 
- * _putchar - write character c to stdio
- * @C: The character to print
- *
+ * _putchar - write character c to stdout
+ * @c: The character to print
  * Return: on success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
+
 {
 	return (write(1, &c, 1));
 }
