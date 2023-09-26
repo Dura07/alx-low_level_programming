338f1a445ebdfd83ff811a8f2e899d84a74cadcf
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-09-25 18:40:37 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int k;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[n])
-				return (s + 1);
+		if (*s == accept[k])
+		return (s);
 		}
+	s++;
 	}
-	return (NULL);
+	return ('\0');
 }
