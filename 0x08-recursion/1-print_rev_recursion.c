961ea0549b949578ce0850da7f308325c69dd787
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-09-28 18:14:03 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,11 +2,13 @@
 
 /**
  * _print_rev_recursion - print a string in reverse
- * @s: the string reverse
+ * @S: the string reverse
  *
  * Return: nothing
  */
+
 void _print_rev_recursion(char *S)
+
 {
 	if (*S == '\0')
 	{
