391f00cc5b243add519cadd1c4212d4f7ee6a38e
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-02 22:42:41 -0700

    2-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
- * main - print its name, followed by a new line
- *
- * @argc: number of command line arguments
- * @argv: array that contain the command line arg..
+ * main - print its name
+ * @argc: arguments count
+ * @argv: argument vector
  *
  * Return: (0) always successful
  */
-int main(int agrc __attribute__((unused)), char *argv[])
-{
 
+int main(int agrc, char *argv[])
+{
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
