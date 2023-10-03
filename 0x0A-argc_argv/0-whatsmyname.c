2916abe24eb6c1c5e60d6ce2bb1e0d10a2d06f08
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-03 01:50:06 -0700

    3-mul.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: (0) always successful
  */
 
-int main(int agrc, char *argv[])
+int main(int argc, char *argv[])
 {
-	(void) argc;
+	(void) argv;
 	printf("%s\n", argv[0]);
 	return (0);
 }
