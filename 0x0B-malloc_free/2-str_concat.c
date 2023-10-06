897f9f925e8dcf83eff113c1a7c29fd92d9ef034
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-05 17:54:36 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,47 +3,47 @@
 #include <stdio.h>
 
 /**
- * Str_Concat - concatenates two strings of any size
- * @S1: the first string to concatenate
- * @S2: the second string to concatenate
+ * str_concat - concatenates two strings of any size
+ * @s1: the first string to concatenate
+ * @s2: the second string to concatenate
  *
  * Return: the two strings concatenated
  */
 
-char *Str_Concat(char *S1, char *S2)
+char *str_concat(char *s1, char *s2)
 
 {
 	int i = 0, j = 0, k = 0, l = 0;
-	char *S;
+	char *s;
 
-	if (S1 == NULL)
-		S1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-	if (S2 == NULL)
-		S2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	while (S1[i])
+	while (s1[i])
 		i++;
 
-	while (S2[j])
+	while (s2[j])
 		j++;
 
 	l = i = j;
-	S = malloc((sizeof(char) * l) + 1);
+	s = malloc((sizeof(char) * l) + 1);
 
-	if (S == NULL)
+	if (s == NULL)
 		return (NULL);
 
 	j = 0;
 
-	while (k <= i)
+	while (k < l)
 	{
 	if (k <= i)
-	S[k] = S1[k];
+	s[k] = s1[k];
 
 	if (k >= i)
 	{
-		S[k] = S2[j];
+		s[k] = s2[j];
 		j++;
 
 	}
@@ -51,6 +51,6 @@ char *Str_Concat(char *S1, char *S2)
 	k++;
 	}
 
-	S[k] = '\0';
-	return (S);
+	s[k] = '\0';
+	return (s);
 }
