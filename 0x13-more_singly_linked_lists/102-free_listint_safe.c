46a4b2e449e64db24ac7369ef934cac1441eefd2
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-23 14:09:13 -0700

    102-free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,21 +10,24 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current = *h;
-	listint_t *temp;
+	listint_t *next_node;
 	size_t count = 0;
 
 	while (current != NULL)
 	{
-		count++;
-		temp = current;
-		current = current->next;
-		free(temp);
+		next_node = current->next;
 
-		if (temp == *h)
+		free(current);
+
+
+		if (current == *h)
 		{
 			*h = NULL;
 			break;
 		}
+
+		current = next_node;
+		count++;
 	}
 
 	return (count);
