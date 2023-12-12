f73d3ac56b7a4b8e778f8eb7e473cd2232729dbf
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-12-12 04:01:00 -0800

    dynamic

--- 0x18-dynamic_libraries/example.py
+++
@@ -1,9 +0,0 @@
-# example.py
-
-import ctypes
-import os
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-libexample = ctypes.CDLL(os.path.join(current_directory, 'libexample.so'))
-
---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between the two integers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of the two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer (numerator).
+ * @b: The second integer (denominator).
+ *
+ * Return: The result of the division.
+ */
+int div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+}
+
+/**
+ * mod - Computes the remainder of the division of two integers.
+ * @a: The first integer (dividend).
+ * @b: The second integer (divisor).
+ *
+ * Return: The remainder of the division.
+ */
+int mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		printf("Error: Modulo by zero\n");
+		return (0);
+	}
+}
